898f44a448eb2dbfc85da8411caa4362f30f7b3d
unknown <brayden.gray98@gmail.com>
2019-07-04 19:18:34 +1200

    project finalised for hand in

--- js/script.js
+++ js/script.js
@@ -44,6 +44,130 @@ var vehicles = [
     litresPerKm: 17
   },
 ]
+var allGyms = [
+  {
+    id: 1,
+    lat: -41.278380,
+    lng: 174.776694,
+    title: "The Beehive",
+  },
+  {
+    id: 2,
+    lat: -41.277147,
+    lng: 174.777076,
+    title: "John Balance Statue",
+  },
+  {
+    id: 3,
+    lat: -41.282068,
+    lng: 174.765446,
+    title: "Floating Globes",
+  },
+  {
+    id: 4,
+    lat: -41.291154,
+    lng: 174.786161,
+    title: "Waterloo Sundial",
+  },
+  {
+    id: 5,
+    lat: -41.296945,
+    lng: 174.79365,
+    title: "Byrd Monument",
+  },
+  {
+    id: 6,
+    lat: -36.845773,
+    lng: 174.754376,
+    title: "Victoria Park Cultivator",
+  },
+  {
+    id: 7,
+    lat: -36.851020,
+    lng: 174.786778,
+    title: "Nancy Steen Garden",
+  },
+  {
+    id: 8,
+    lat: -36.874670,
+    lng: 174.763921,
+    title: "Langton's Tea Kiosk",
+  },
+  {
+    id: 9,
+    lat: -36.864519,
+    lng: 174.757213,
+    title: "Basque Park Fountain",
+  },
+  {
+    id: 10,
+    lat: -36.850870,
+    lng: 174.725125,
+    title: "Cox's Bay Reserve",
+  }
+]
+var allNests = [
+  {
+    id: 1,
+    lat: -39.0556,
+    lng: 174.0752,
+    title: "New Plymouth",
+  },
+  {
+    id: 2,
+    lat: -40.3523,
+    lng: 175.6082,
+    title: "Palmerston North",
+  },
+  {
+    id: 3,
+    lat: -41.2865,
+    lng: 174.7762,
+    title: "Wellington",
+  },
+  {
+    id: 4,
+    lat: -36.8485,
+    lng: 174.7633,
+    title: "Auckland",
+  },
+  {
+    id: 5,
+    lat: -41.2706,
+    lng: 173.2840,
+    title: "Nelson",
+  },
+  {
+    id: 6,
+    lat: -38.6857,
+    lng: 176.0702,
+    title: "Taupo",
+  },
+  {
+    id: 7,
+    lat: -39.4928,
+    lng: 176.9120,
+    title: "Napier",
+  },
+  {
+    id: 8,
+    lat: -43.5321,
+    lng: 172.6362,
+    title: "Christchurch",
+  },
+  {
+    id: 9,
+    lat: -45.8788,
+    lng: 170.5028,
+    title: "Dunedin",
+  },
+  {
+    id: 10,
+    lat: -37.6878,
+    lng: 176.1651,
+    title: "Tauranga",
+  }
+]
 var vehicleSelect = document.getElementById('vehicleSelect');
 var selectedVehicle;
 var vehicleCost;
@@ -77,9 +201,7 @@ function chosenVehicle(vehicleId){
   fuelDistance = selectedVehicle.litresPerKm;
   console.log(fuelDistance + "L/100km");
 
-
 }
-
 
 
 $("#panelChange").click(function(){
@@ -93,6 +215,8 @@ $("#panelChange").click(function(){
 });
 
 $("#submitTrip").click(function(){
+
+
 
 
   if (x.matches) {
@@ -125,14 +249,98 @@ var numOfPeople;
 var numOfDays;
 var distance
 var map;
+var activeMarkers = [];
+var activeMarkers2 = [];
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: -41.279078, lng: 174.780281},
     zoom: 8
   });
+  for (var i = 0; i < allGyms.length; i++) {
 
+    var marker = new google.maps.Marker({
+      position:{
+        lat: allGyms[i].lat,
+        lng: allGyms[i].lng
+      },
+      map: map,
+      animation: google.maps.Animation.DROP,
+      icon: 'assets/gymIcon.png',
+      markerTitle: allGyms[i].title,
+      markerID: allGyms[i].id
+    });
+    activeMarkers.push(marker);
+  }
 
+  $('#showExGyms').change(function(){
+      console.log('yeet');
+      var e = document.getElementById("showExGyms");
+      var strUser = e.options[e.selectedIndex].value;
+      console.log(strUser);
+      if (strUser == "No") {
+        for (var i = 0; i < activeMarkers.length; i++) {
+          activeMarkers[i].setMap(null);
+        }
+        activeMarkers = [];
+      } else if (strUser == "Yes") {
+        for (var i = 0; i < allGyms.length; i++) {
+          var marker = new google.maps.Marker({
+            position:{
+              lat: allGyms[i].lat,
+              lng: allGyms[i].lng
+            },
+            map: map,
+            animation: google.maps.Animation.DROP,
+            icon: 'assets/gymIcon.png',
+            markerTitle: allGyms[i].title,
+            markerID: allGyms[i].id
+          });
+          activeMarkers.push(marker);
+        }
+      }
+  });
+  for (var i = 0; i < allNests.length; i++) {
 
+    var marker = new google.maps.Marker({
+      position:{
+        lat: allNests[i].lat,
+        lng: allNests[i].lng
+      },
+      map: map,
+      animation: google.maps.Animation.DROP,
+      icon: 'assets/pokemonIcon.png',
+      markerTitle: allNests[i].title,
+      markerID: allNests[i].id
+    });
+    activeMarkers2.push(marker);
+  }
+  $('#showPokemon').change(function(){
+      console.log('yeet');
+      var a = document.getElementById("showPokemon");
+      var strUser2 = a.options[a.selectedIndex].value;
+      console.log(strUser2);
+      if (strUser2 == "False") {
+        for (var i = 0; i < activeMarkers2.length; i++) {
+          activeMarkers2[i].setMap(null);
+        }
+        activeMarkers2 = [];
+      } else if (strUser2 == "True") {
+        for (var i = 0; i < allGyms.length; i++) {
+          var marker = new google.maps.Marker({
+            position:{
+              lat: allNests[i].lat,
+              lng: allNests[i].lng
+            },
+            map: map,
+            animation: google.maps.Animation.DROP,
+            icon: 'assets/pokemonIcon.png',
+            markerTitle: allNests[i].title,
+            markerID: allNests[i].id
+          });
+          activeMarkers2.push(marker);
+        }
+      }
+  });
 
   var input = document.getElementById('startLocation');
   var autoComplete = new google.maps.places.Autocomplete(input);
@@ -231,7 +439,7 @@ numOfDaysJS.addEventListener("input", function(){
   validateVehicles();
 });
 function validateVehicles(){
-    document.getElementById("vehicleSelect").innerHTML = "";
+  document.getElementById("vehicleSelect").innerHTML = "";
   for (var i = 0; i < vehicles.length; i++) {
     if ((peopleValue >= vehicles[i].minPeople && peopleValue <= vehicles[i].maxPeople) && (daysValue >= vehicles[i].minDays && daysValue <= vehicles[i].maxDays)) {
       console.log(vehicles[i]);
